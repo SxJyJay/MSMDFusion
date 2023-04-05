a84731f461dc3d759c3b6653a611c7747f2f2ae0
Jay <sxjyjay@163.com>
2023-04-05 14:16:51 +0800

    Remove redundant code

--- mmdet3d/datasets/pipelines/__init__.py
+++ mmdet3d/datasets/pipelines/__init__.py
@@ -4,14 +4,9 @@ from .formating import Collect3D, DefaultFormatBundle, DefaultFormatBundle3D
 from .loading import (LoadAnnotations3D, LoadMultiViewImageFromFiles,
                       LoadPointsFromFile, LoadPointsFromMultiSweeps,
                       NormalizePointsColor, PointSegClassMapping)
-# from .my_loading import (LoadForeground2D, LoadForeground2DFromMultiSweeps, 
-#                         GlobalRotTransFilterForeground2D, ImgScaleCropFlipForeground2D, 
-#                         MyCollect3D)
 from .my_loading_multi_proj import (LoadForeground2D, LoadForeground2DFromMultiSweeps, 
                         GlobalRotTransFilterForeground2D, ImgScaleCropFlipForeground2D, 
                         MyCollect3D, ShuffleForeground2D)
-from .my_loading_DC import (LoadForeground2D_DC, LoadForeground2DFromMultiSweeps_DC,
-                        GlobalRotTransFilterForeground2D_DC)
 from .test_time_aug import MultiScaleFlipAug3D
 from .transforms_3d import (BackgroundPointsFilter, GlobalRotScaleTrans,
                             IndoorPointSample, ObjectNoise, ObjectRangeFilter,
@@ -27,6 +22,5 @@ __all__ = [
     'PointSegClassMapping', 'MultiScaleFlipAug3D', 'LoadPointsFromMultiSweeps',
     'BackgroundPointsFilter', 'VoxelBasedPointSampler', 'LoadForeground2D', 'LoadForeground2DFromMultiSweeps',
     'LoadForeground2D', 'LoadForeground2DFromMultiSweeps', 'GlobalRotTransFilterForeground2D', 
-    'MyCollect3D', 'ImgScaleCropFlipForeground2D', 'LoadForeground2D_DC', 'LoadForeground2DFromMultiSweeps_DC',
-    'GlobalRotTransFilterForeground2D_DC', 'ShuffleForeground2D'
+    'MyCollect3D', 'ImgScaleCropFlipForeground2D','ShuffleForeground2D'
 ]
