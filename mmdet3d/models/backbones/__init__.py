cf02d3ba6cf2ff79d4e9cff87d1482b45fdc9c47
Jay <sxjyjay@163.com>
2023-04-08 14:09:11 +0800

    fix import bug

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -5,9 +5,8 @@ from .pointnet2_sa_msg import PointNet2SAMSG
 from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
 from .DLA import DLASeg
-from .cbnet import CBSwinTransformer
+
 __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
-    'SECOND', 'PointNet2SASSG', 'PointNet2SAMSG', 'MultiBackbone', 'DLASeg',
-    'CBSwinTransformer'
+    'SECOND', 'PointNet2SASSG', 'PointNet2SAMSG', 'MultiBackbone', 'DLASeg'
 ]
